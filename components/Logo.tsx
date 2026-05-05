6fe3d365c34e35dc1444cccf2862e643fbf46231
deveshagre <deveshagre50@gmail.com>
2026-05-05 23:53:10 +0530

    change: Size of logo in header

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export function Logo() {
   return (
     <Link href="/" className="focus-ring inline-flex items-center gap-2 rounded-md">
-      <Image 
-        src="/header-logo.png" 
-        alt="Nexora Logo" 
-        width={140} 
-        height={50} 
-        className="object-contain" 
-        priority 
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/header-logo.png"
+        alt="Nexora Logo"
+        width={140}
+        height={50}
+        className="object-contain"
       />
     </Link>
   );
